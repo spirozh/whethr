dc8023fb13411e350d629915b5eb3e47188c92fc
Jason Songhurst <jason.songhurst@gmail.com>
2021-03-16 14:19:59 -0700

    add app

---
+++ whethr/config.py
@@ -0,0 +1,29 @@
+import os
+
+ENV = os.environ.get("ENV", "development")
+
+# Statement for enabling the development environment
+DEBUG = (ENV == "development")
+
+# Define the application directory
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+# Enable protection against *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data. 
+CSRF_SESSION_KEY = "super_secret"
+
+# Secret key for signing cookies
+SECRET_KEY = "cookie_secret"
+
+# API keys
+OWM_KEY = "d5d1a49b1fdf3b6e04e1add6983ef4f7"
+IPSTACK_KEY = "e54b84d449bb5f90092b0f575dd41fad"
---
+++ whethr/routes.py
@@ -0,0 +1,72 @@
+# Import flask and template operators
+from flask import Flask, render_template, redirect, flash, request
+from pyowm import OWM
+from ipstack import GeoLookup
+
+from .config import *
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# get the location from a query parameter (should be: "<lat>,<lon>")
+def loc_from_query(lat, lon):
+    return float(lat), float(lon)
+
+# get the location related to an ip address
+def loc_from_ip(ip):
+    geo = GeoLookup(IPSTACK_KEY)
+    loc = geo.get_location(ip)
+    return loc['latitude'], loc['longitude']
+
+# ensure the location is valid
+def ensure_valid_loc(loc):
+    lat, lon = loc
+    if lat is None:
+        lat = 33.9383776
+    if lon is None:
+        lon = -118.3111258)
+
+    return (lat, lon)
+
+# get the weather for a given location
+def weather_for_loc(loc):
+    lat, lon = loc
+
+    mgr = OWM(OWM_KEY).weather_manager()
+    observation = mgr.weather_at_coords(lat, lon)
+
+    return observation.weather
+
+# determine hat necessity given the weather
+def a_hat_is_needed(weather):
+    temp_feels_like = weather.temperature("fahrenheit")['feels_like']
+    rain = weather.rain
+    snow = weather.snow
+    
+    return (temp_feels_like < 60) or rain or snow
+
+
+@app.route('/i_should_wear_a_hat')
+def i_should_wear_a_hat():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    ip = request.remote_addr
+
+    loc = loc_from_query(lat, lon) if lat and lon else loc_from_ip(ip)
+    loc = ensure_valid_loc(loc)
+    
+    weather = weather_for_loc(loc)
+
+    should = "should" if a_hat_is_needed(weather) else "should not"
+    
+    return render_template('i_should_wear_a_hat.html',
+                           should=should, weather=weather), 200
+
+@app.route('/')
+def root():
+    return redirect('/i_should_wear_a_hat', code=302)
+
+# 404 errror handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
